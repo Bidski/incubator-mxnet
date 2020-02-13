ccee63a8b8f598d44c1b5ca01ece3ee4832c5661
Alex Biddulph <c3124185@uon.edu.au>
2020-02-13 15:36:08 +1100

    Clean up 3rdparty builds

--- 3rdparty/cmake/Buildonnx-tensorrt.cmake
+++ 3rdparty/cmake/Buildonnx-tensorrt.cmake
@@ -19,27 +19,7 @@ ExternalProject_Add(
              -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS}
              -DBUILD_ONNX_PYTHON=ON
              -DONNX_ML=ON
-  BUILD_COMMAND ${CMAKE_MAKE_PROGRAM}
+  BUILD_COMMAND ${CMAKE_MAKE_PROGRAM} third_party/onnx/all all
   INSTALL_DIR ${CMAKE_INSTALL_PREFIX}
-  INSTALL_COMMAND ${CMAKE_MAKE_PROGRAM} install
+  INSTALL_COMMAND ${CMAKE_MAKE_PROGRAM} third_party/onnx/install install
   TEST_COMMAND "")
-
-ExternalProject_Get_Property(onnxtensorrt SOURCE_DIR BINARY_DIR)
-
-ExternalProject_Add_Step(
-  onnxtensorrt pre_build
-  COMMAND ${CMAKE_COMMAND} -E make_directory
-          ${SOURCE_DIR}/third_party/onnx/build
-  COMMAND
-    ${CMAKE_COMMAND} -E chdir ${SOURCE_DIR}/third_party/onnx/build
-    ${CMAKE_COMMAND} -G${CMAKE_GENERATOR} -DCMAKE_INSTALL_LIBDIR=lib
-    -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release
-    -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
-    -DCMAKE_CXX_FLAGS=${CXX_FLAGS} -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS}
-    -DBUILD_ONNX_PYTHON=ON ${SOURCE_DIR}/third_party/onnx
-  COMMAND ${CMAKE_COMMAND} -E chdir ${SOURCE_DIR}/third_party/onnx/build
-          ${CMAKE_MAKE_PROGRAM}
-  COMMAND ${CMAKE_COMMAND} -E chdir ${SOURCE_DIR}/third_party/onnx/build
-          ${CMAKE_MAKE_PROGRAM} install
-  DEPENDEES patch
-  DEPENDERS build)
